c66b830819c6f71c72380c99229bbe7beb3b8a74
Matt Shilling <mattthewshilling@gmail.com>
2023-04-09 03:57:55 -0700

    Formatting.

--- apps/36hr-watch/36hr.h
+++ apps/36hr-watch/36hr.h
@@ -1,16 +1,15 @@
 #pragma once
 
-#include <stdint.h>
 #include "watch_rtc.h"
+#include <stdint.h>
 
 typedef struct {
-    uint32_t second;    // 0-59
-    uint32_t minute;    // 0-59
-    uint32_t hour;      // 0-36
-    uint32_t day;       // 1-20
-    uint32_t month;     // 1-12
-    uint32_t year;      // 0-63 (representing 2020-2083)
+  uint32_t second; // 0-59
+  uint32_t minute; // 0-59
+  uint32_t hour;   // 0-36
+  uint32_t day;    // 1-20
+  uint32_t month;  // 1-12
+  uint32_t year;   // 0-63 (representing 2020-2083)
 } watch_36hr_date_time;
 
 watch_36hr_date_time watch_date_time_to_36hr(const watch_date_time time);
-
--- apps/36hr-watch/app_board.h
+++ apps/36hr-watch/app_board.h
@@ -1,46 +1,49 @@
 #pragma once
 
+#include <stdint.h>
+
 /**
  * @brief Internal application data.
  */
 typedef struct AppBoard {
-    /**
-     * Current mode.
-     */
-    AppMode mode;
+  /**
+   * Current mode.
+   */
+  AppMode mode;
 
-    /**
-     * Lets us perform one-time setup for a given mode.
-     */
-    bool mode_changed;
+  /**
+   * Lets us perform one-time setup for a given mode.
+   */
+  bool mode_changed;
 
-    /**
-     * Timeout for the SET mode (returns to 24hr clock after timeout expires).
-     */
-    uint16_t mode_ticks;
+  /**
+   * Timeout for the SET mode (returns to 24hr clock after timeout expires).
+   */
+  uint16_t mode_ticks;
 
-    /**
-     * Timeout for the light.
-     */
-    uint8_t light_ticks;
+  /**
+   * Timeout for the light.
+   */
+  uint8_t light_ticks;
 
-    /**
-     * Indicates that the LED is on.
-     */
-    bool led_on;
+  /**
+   * Indicates that the LED is on.
+   */
+  bool led_on;
 
-    /**
-     * Tracks the current page in log, prefs or settings.
-     */
-    uint8_t page;
+  /**
+   * Tracks the current page in log, prefs or settings.
+   */
+  uint8_t page;
 
-    /**
-     * Lets us see when the second changed, for subsecond timing.
-     */
-    uint8_t last_second;
+  /**
+   * Lets us see when the second changed, for subsecond timing.
+   */
+  uint8_t last_second;
 
-    /**
-     * A value from 0 to (BEAT_REFRESH_FREQUENCY - 1) indicating the fractional second.
-     */
-    uint8_t subsecond;
+  /**
+   * A value from 0 to (BEAT_REFRESH_FREQUENCY - 1) indicating the fractional
+   * second.
+   */
+  uint8_t subsecond;
 } AppBoard;
